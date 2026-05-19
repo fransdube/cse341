eb0a0f33b5e864a0f3c903f105eaf7c5a935cdf0
fransdube <31796906+fransdube@users.noreply.github.com>
2026-05-19 14:19:12 +0000

    fix: resolve swagger schemes merge conflict

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API for managing contacts'
   },
   host: 'cse341-fransdube.onrender.com',
-  schemes: ['https']
+  schemes: ['https', 'http']
 };
 
 const outputFile = './swagger.json';
